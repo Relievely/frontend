b71d39737a33abc48ecffc04119ad36328734ced
userxperience <tijnvisser3@gmail.com>
2022-11-22 13:03:06 +0100

    added settings page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { getIndex } from '../controllers/indexController.js';
+import { getSettings } from '../controllers/settingsController.js';
 
 const router = Router();
 
 // all routes
 router.get('/', getIndex);
+router.get('/settings', getSettings);
 
 export default router;
