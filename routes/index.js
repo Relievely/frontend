8a8e0241685d028bc01c0c0c20031691264dc5e5
Paolina Ganeva <paolinaganeva@gmail.com>
2022-11-23 11:18:55 +0100

    Added the home and wiki page files

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import { getIndex } from '../controllers/indexController.js';
 import { getSettings } from '../controllers/settingsController.js';
+import {getHome} from "../controllers/homeController.js";
+import {getWiki} from "../controllers/wikiController.js";
 
 const router = Router();
 
 // all routes
 router.get('/', getIndex);
 router.get('/settings', getSettings);
+router.get('/home', getHome);
+router.get('/wiki', getWiki);
 
 export default router;
