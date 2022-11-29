e646e768dacb0a29250787d426d261333bc9f76d
userxperience <tijnvisser3@gmail.com>
2022-11-29 14:57:03 +0100

    input field on background page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import useSWR from "swr";
 import {
   StyleSheet,
@@ -6,10 +6,10 @@ import {
   Text,
   TextInput,
   Button,
-  FlatList,
   ImageBackground,
 } from "react-native";
 import * as path from "path";
+import { SafeAreaView } from "react-native-web";
 
 const fetcher = () =>
   fetch("/api/scores/list").then((response) => response.json());
@@ -18,15 +18,22 @@ export default function Home() {
   const { data, mutate } = useSWR("", fetcher);
   const [name, setName] = useState("");
   const [value, setValue] = useState("");
-  const source={ uri: require('../public/backgroundLight.jpg') };
-console.log()
+  const source={ uri: require('../public/backgroundDark.jpg') };
+  const initialUsername = "Guest"; //the username the user fills in in the guide
+  const [userName, onChangeText] = React.useState(initialUsername);
+  console.log(userName)
   return (
-    <View style={styles.container}>
-      <ImageBackground source={source.uri.default.src} style={styles.image}>
-      </ImageBackground>
-    </View>
+    <ImageBackground source={source.uri.default.src} style={styles.bgImg}>
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={userName}
+      />
+    </SafeAreaView>
+    </ImageBackground>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -44,5 +51,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     backgroundColor: "#000000c0"
-  }
+  },
+  bgImg: {
+    height: "448px",
+    width: "207px",
+  },
 });
