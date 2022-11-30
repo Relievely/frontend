2b33297857538d4e7c29be7341dcb1bb67adfce1
Paolina Ganeva <paolinaganeva@gmail.com>
2022-11-30 15:34:12 +0100

    Added a base for the journal page

---
+++ README.md
@@ -0,0 +1,3 @@
+I'm using for my part which is the journal page the documentation from these websites
+<li>https://v5.reactrouter.com/native/example/Basic</li>
+<li>https://reactnative.dev/docs/intro-react</li>
---
+++ pages/journal.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import {
+    Text,
+    View,
+} from "react-native-web";
+
+const component1 = () => {
+    return (
+        <View></View>
+    );
+}
+
+export default component1();
